21fbd3f6f2f129da571cc4d0c5ea162b7944b0aa
Joe Gomain Hoyes <gomain@gmail.com>
2021-06-14 18:47:30 +0700

    `Math`'s operations type

--- src/Data/Math.purs
+++ src/Data/Math.purs
@@ -1,12 +1,13 @@
 module Data.Math where
 
-import Prelude
-
-import Partial.Unsafe (unsafePartial)
+import Prelude hiding (add, sub, mul, div)
 
 data Math a
   = Lit a
-  | Add (Math a) (Math a)
+  | Op (Operation a)
+
+data Operation a
+  = Add (Math a) (Math a)
   | Sub (Math a) (Math a)
   | Mul (Math a) (Math a)
   | Div (Math a) (Math a)
@@ -14,15 +15,29 @@ data Math a
 
 instance Show a => Show (Math a) where
   show (Lit a) = show a
-  show grouped = "(" <> unsafePartial showOp grouped <> ")"
+  show (Op operation) = "(" <> showOp operation <> ")"
     where
-      showOp :: Partial => forall a. Show a => Math a -> String
+      showOp :: Operation a -> String
       showOp (Add l r) = show l <> " + " <> show r
       showOp (Sub l r) = show l <> " - " <> show r
       showOp (Mul l r) = show l <> " * " <> show r
       showOp (Div l r) = show l <> " / " <> show r
       showOp (Neg e) = "-" <> show e
 
+add :: forall a. Math a -> Math a -> Math a
+add l r = Op (Add l r)
+
+sub :: forall a. Math a -> Math a -> Math a
+sub l r = Op (Sub l r)
+
+mul :: forall a. Math a -> Math a -> Math a
+mul l r = Op (Mul l r)
+
+div :: forall a. Math a -> Math a -> Math a
+div l r = Op (Mul l r)
+
+neg :: forall a. Math a -> Math a
+neg = Op <<< Neg
 
 --|          (+)
 --|         /   \
@@ -33,13 +48,13 @@ instance Show a => Show (Math a) where
 --| 1   2   3   4    5
 expr :: Math Int
 expr
-  = Add
-     (Sub
-      (Mul
+  = add
+     (sub
+      (mul
        (Lit 1)
        (Lit 2))
-      (Div
+      (div
        (Lit 3)
        (Lit 4)))
-     (Neg
+     (neg
       (Lit 5))
