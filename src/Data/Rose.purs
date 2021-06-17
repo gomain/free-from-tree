64149c4a08b991d2f273d2b8d56013078f0e6ffb
Joe Gomain Hoyes <gomain@gmail.com>
2021-06-18 00:18:43 +0700

    rose tree dose not need a leaf constructor

--- src/Data/Rose.purs
+++ src/Data/Rose.purs
@@ -4,29 +4,28 @@ import Prelude
 
 import Data.Array as A
 import Data.Foldable (class Foldable)
-import Data.List (List)
+import Data.List (List(..))
 import Data.List as L
 
 data Rose a
-  = Leaf a
-  | Branch (List (Rose a))
+  = Rose a (List (Rose a))
 
 instance Show a => Show (Rose a) where
-  show (Leaf a) = show a
-  show (Branch l) = show <<< A.fromFoldable $ l
+  show (Rose a Nil) = show a
+  show (Rose a l) = "[" <> show a <> "," <> (show <<< A.fromFoldable $ l) <> "]"
 
 leaf :: forall a. a -> Rose a
-leaf = Leaf
+leaf = flip Rose Nil
 
-branch :: forall f a. Foldable f => f (Rose a) -> Rose a
-branch = Branch <<< L.fromFoldable
+branch :: forall f a. Foldable f => a -> f (Rose a) -> Rose a
+branch a ch = Rose a $ L.fromFoldable ch
 
 rose :: Rose Int
-rose = branch
-       [ branch
+rose = branch 10
+       [ branch 20
          [ leaf 1
          , leaf 2
-         , branch
+         , branch 30
            [ leaf 3
            , leaf 4
            ]
