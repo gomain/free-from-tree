25556eef59c15bb6d17fc4518c38b319a00859c8
Joe Gomain Hoyes <gomain@gmail.com>
2021-06-18 23:27:26 +0700

    compose `Pair` and `Maybe` (#18)

--- src/Data/BST.purs
+++ src/Data/BST.purs
@@ -2,21 +2,24 @@ module Data.BST where
 
 import Prelude
 
+import Data.Functor.Compose (Compose)
 import Data.Maybe (Maybe(..), maybe)
+import Data.Newtype (unwrap, wrap)
 import Data.Tree (Pair(..))
 
 --| a binary search tree
-data BST a = BST a (Pair (Maybe (BST a)))
+data BST a = BST a (Compose Pair Maybe (BST a))
 
 instance Show a => Show (BST a) where
-  show (BST a (Pair l r))
-    = "[" <> maybe "Nothing" show l <> ", " <> show a <> ", " <> maybe "Nothing" show r <> "]"
+  show (BST a cmp)
+    = let Pair l r = unwrap cmp
+      in "[" <> maybe "Nothing" show l <> ", " <> show a <> ", " <> maybe "Nothing" show r <> "]"
 
 leaf :: forall a. a -> BST a
-leaf a = BST a (Pair Nothing Nothing)
+leaf a = BST a $ wrap (Pair Nothing Nothing)
 
 branch :: forall a. a -> BST a -> BST a -> BST a
-branch a l r = BST a (Pair (Just l) (Just r))
+branch a l r = BST a $ wrap (Pair (Just l) (Just r))
 
 bst :: BST Int
 bst = branch 4
